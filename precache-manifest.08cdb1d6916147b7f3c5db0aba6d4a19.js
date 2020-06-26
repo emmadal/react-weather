5c97bfb25ea7016e1354e9a702e0842b71829d1e
emmadal <emmanueldalougou@gmail.com>
2020-06-26 13:24:37 +0000

    Updates

---
+++ precache-manifest.08cdb1d6916147b7f3c5db0aba6d4a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86dca41aac18b7a6710d53072ce1972",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "8cb1792884cc147c2b9e",
+    "url": "/react-weather/static/css/main.64d62cce.chunk.css"
+  },
+  {
+    "revision": "b09f08b1303bca04687b",
+    "url": "/react-weather/static/js/2.abd78658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-weather/static/js/2.abd78658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb1792884cc147c2b9e",
+    "url": "/react-weather/static/js/main.31089bf4.chunk.js"
+  },
+  {
+    "revision": "2976991085f74c894ca6",
+    "url": "/react-weather/static/js/runtime-main.9b356376.js"
+  }
+]);
